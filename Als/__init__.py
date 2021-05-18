de70e3af1a5cded6e465785dee197929872a4328
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-05-19 02:47:34 +0400

    Update __init__.py

--- Als/__init__.py
+++ Als/__init__.py
@@ -1,1 +1,12 @@
 # __init__.py
+
+"""
+Top-level package for Als
+"""
+
+__name__ = "Als"
+__description__ = "Advanced version of Bash's \"ls\" command"
+__doc__ = "../docs/"
+__author__ = "Luka Mamukashvili (UltraStudioLTD)"
+__package__ = "Als"
+__version__ = "0.0.1"
