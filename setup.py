3c3667f1d0d4d427650d339b85a1cdc35d587552
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-05-21 00:24:39 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='Als',
+    version='0.0.1',
+    description="Python-based Advanced version of Bash's \"ls\"",
+    author='UltraStudioLTD (Luka Mamukashvili)',
+    author_email='ultraluka0@gmail.com',
+    url='https://www.github.com/UltraStudioLTD/Als',
+    packages=[
+        'Als',
+        ],
+    scripts=[
+        'Als/ls.py',
+        'Als/tree.py',
+        'Als/runner.py',
+        'Als/Als.py',
+        'Als/__init__.py',
+        'Als/colors.py',
+        'Als/permissions.py'
+        ]
+)
